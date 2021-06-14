c880509fa7cfc932e409697e5ea73ea5251358d3
worldtechtube <worldtechtube007@gmail.com>
2021-06-14 12:44:28 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+  ],
+  providers: [HttpClientModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+ @Input() input1: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
